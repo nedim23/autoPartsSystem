ba0baa765e9e91be56587107a23de177f5a03f8d
Nedim Omerbegovic <nedim.omerbegovic@comtrade.com>
2016-11-27 11:36:20 +0100

    Add logger.

--- autoParts/routes/task.js
+++ autoParts/routes/task.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var logger = require('../util/logger');
 
 router.post('/task', function (req, res, next) {
+    logger.info("Create new task...");
     var payload = req.body;
+    logger.info("Request body: ", JSON.stringify(payload))
     console.log("Task payload: ", JSON.stringify(payload));
     models.task.build({
         name: payload.name,
-        description: payload.description
+        description: payload.description,
+        status: payload.status
     })
         .save()
-        .then(function (task){
+        .then(function (task) {
 
+        })
+        .catch(function (err) {
+            logger.error("Error creating task: ", err);
         });
 });
 
